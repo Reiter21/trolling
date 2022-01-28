0aaa05713be3ccf78b752fceb6c5583217246229
Reiter21 <palash21gupta@gmail.com>
2022-01-28 22:03:55 +0530

    xd

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ PORT = 5000
 def home():
     return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
 
+@app.errorhandler(404)
+def not_found(e):  
+  return redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+
+
 
 if __name__ == "__main__":
     app.run()
