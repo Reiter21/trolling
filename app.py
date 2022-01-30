ebd92950a76c8a83828a083dc58408b37df28a4a
Reiter21 <palash21gupta@gmail.com>
2022-01-30 11:31:46 +0530

    trolling

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, redirect
-import logging
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ok'
-log = logging.getLogger('werkzeug')
-log.disabled = True
-SESSION_COOKIE_SECURE = True
-PORT = 5000
-
 
 @app.route('/')
 def home():
